607f8262e835d29b8f2eb4ba679d1463caf4270a
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-13 13:17:17 -0700

    Conditional Statements

---
+++ file1.js
@@ -0,0 +1,10 @@
+let name = "Masino";
+let num = '9036206457';
+
+if name = "Masino" {
+	alert("My name is 'Masino'");
+	alert("Done from here");
+} else {
+	alert("Name is not Masino");
+	alert("Chose a new name...");
+}

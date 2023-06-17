cd3962d972bf02735a29d3c0d21b1e5e3cd57954
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-17 01:46:15 -0700

    Iterator

---
+++ more.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function product() {
+	let fruits = ['apple', 'mango', 'guava'];
+	for fruit in fruits {
+		console.log(fruit);
+	}
+	console.log('We are done here');
+};
+console.log('Code by Masino');
